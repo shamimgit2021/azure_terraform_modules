9ab5c872fb1feaa4c5be8d24a595bcd8c2110c92
shamimgit2021 <91812554+shamimgit2021@users.noreply.github.com>
2026-02-12 13:55:43 +0530

    Add Azure provider configuration in provider.tf

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.63.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+  subscription_id            = "4a25f603-dcab-4159-96fc-edb1938e019e"
+  tenant_id                  = "407dfb04-b176-4113-8576-11ade6dcd85b"
+  client_id                  = "8e7c6fc2-6b15-456c-a1c8-0bb82be72dec"
+  client_secret              = "CMp8Q~ZtCvWiMkdTaN~lrudKKm6VBLYOgnoiibQa"
+  skip_provider_registration = true
+}
