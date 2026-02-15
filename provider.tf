6058c106ffe67d954f6094d88b07957a745fedfb
shamimgit2021 <91812554+shamimgit2021@users.noreply.github.com>
2026-02-15 23:59:04 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -14,6 +14,6 @@ provider "azurerm" {
   subscription_id            = "4a25f603-dcab-4159-96fc-edb1938e019e"
   tenant_id                  = "407dfb04-b176-4113-8576-11ade6dcd85b"
   client_id                  = "8e7c6fc2-6b15-456c-a1c8-0bb82be72dec"
-  client_secret              = "CMp8Q~ZtCvWiMkdTaN~lrudKKm6VBLYOgnoiibQa"
+  client_secret              = "261a9785-393f-44f2-a233-c31aaa9f35f4"
   skip_provider_registration = true
 }
