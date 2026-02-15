ceeeea75f2fcd7e0275c1c59d708651b33074b2f
shamimgit2021 <91812554+shamimgit2021@users.noreply.github.com>
2026-02-15 23:00:48 +0530

    Update linuxvm.tf

--- linuxvm.tf
+++ linuxvm.tf
@@ -16,12 +16,6 @@ resource "azurerm_subnet" "test1" {
   virtual_network_name = azurerm_virtual_network.test.name
   address_prefixes     = ["10.0.1.0/24"]
 }
-resource "azurerm_subnet" "test2" {
-  name                 = "test-sub2"
-  resource_group_name  = azurerm_resource_group.test.name
-  virtual_network_name = azurerm_virtual_network.test.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
 
 resource "azurerm_network_interface" "test" {
   name                = "test-nic"
